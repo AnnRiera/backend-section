130af97459fdcfc9914025546ee0d71c57185202
AnnRiera <AnnmRiera@gmail.com>
2020-06-28 11:15:06 -0400

    Jest implementation

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+    user: {
+        "_id": "5ef896d1d2b75b25a83d9114",
+        "name": "Ana Riera",
+        "username": "Ana",
+        "password": "861995"
+    },
+    users: [
+        {
+            _id: "507f191e810c19729de860ea",
+            name: "Marluan",
+            username: "Marluan03",
+            password: "mystrongPassword"
+        },
+        {
+            _id: "507f191e810c19729de860eb",
+            name: "Erick",
+            username: "Erick_34",
+            password: "mystrongPassword"
+        }
+    ]
+};
