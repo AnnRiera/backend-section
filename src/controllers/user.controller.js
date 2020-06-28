f4da7c41ade42c7c492cb9f5dab9f4428d779c97
AnnRiera <AnnmRiera@gmail.com>
2020-06-28 09:14:44 -0400

    Authentication implement

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,31 +1,31 @@
-let _userservice = null;
-
+let _userService = null;
 class UserController {
     constructor({ UserService }) {
-        _userservice = UserService; //Sin el this es tipo privado.
+        _userService = UserService; //Sin el this es un repo privado.
     }
 
     async get(req, res) {
-        const {userId} = req.params; 
-        const user = await _userservice.get(userId);
+        const { userId } = req.params;
+        const user = await _userService.get(userId);
         return res.send(user);
     }
 
     async getAll(req, res) {
-        const users = await _userservice.getAll();
+        const { pageSize, pageNum } = req.query;
+        const users = await _userService.getAll(pageSize, pageNum);
         return res.send(users);
     }
 
     async update(req, res) {
         const { body } = req;
-        const userId = req.params;
-        const updatedUser = await _userservice.update(userId, body);
+        const { userId } = req.params;
+        const updatedUser = await _userService.update(userId, body);
         return res.send(updatedUser);
     }
 
     async delete(req, res) {
         const { userId } = req.params;
-        const deletedUser = await _userservice.delete(userId);
+        const deletedUser = await _userService.delete(userId);
         return res.send(deletedUser);
     }
 }
--- src/services/base.service.js
+++ src/services/base.service.js
@@ -1,52 +1,55 @@
 class BaseService {
-    constructor(repository){
+    constructor(repository) {
         this.repository = repository;
     }
 
-    async get(id){
-        if (!id){
+    async get(id) {
+        if (!id) {
             const error = new Error();
             error.status = 400;
-            error.message = "id mus be sent";
+            error.message = "id must be sent";
             throw error;
         }
 
         const currentEntity = await this.repository.get(id);
 
-        if (!currentEntity){
+        if (!currentEntity) {
             const error = new Error();
-            error.status = 400;
+            error.status = 404;
             error.message = "entity does not found";
             throw error;
         }
+
         return currentEntity;
     }
 
-    async getAll(){
-        return await this.repository.getAll();
+    async getAll(pageSize, pageNum) {
+        return await this.repository.getAll(pageSize, pageNum);
     }
 
-    async create(entity){
+    async create(entity) {
         return await this.repository.create(entity);
     }
 
-    async update(id, entity){
+    async update(id, entity) {
         if (!id) {
             const error = new Error();
             error.status = 400;
-            error.message = "id mus be sent";
+            error.message = "id must be sent";
             throw error;
         }
+
         return await this.repository.update(id, entity);
     }
 
-    async delete(id){
+    async delete(id) {
         if (!id) {
             const error = new Error();
             error.status = 400;
-            error.message = "id mus be sent";
+            error.message = "id must be sent";
             throw error;
         }
+
         return await this.repository.delete(id);
     }
 }
